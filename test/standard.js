8d2be284d0e9e60af752b1b1acd7b37750ad4ee2
matthewalbani <Matthew.jalbani@gmail.com>
2016-05-03 18:14:08 -0700

    get passing tests

--- test/standard.js
+++
@@ -1,9 +0,0 @@
-/* global it, describe */
-var semistandard = require('mocha-standard/semistandard');
-
-describe('coding style', function () {
-  this.timeout(5000);
-  it('conforms to semistandard', semistandard.files([
-    'index.js', 'test/*.js'
-  ]));
-});
